98f0d905d3c3fd92555f0f32aa9d2b5916de10e1
davidgiraldovalencia <dagiva25@gmail.com>
2025-11-22 08:42:13 -0500

    actualizar archivo main y reorganizar archivos

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,30 +1,43 @@
 # obtain a list of files in the input directory
 import os
 
-from .write_count_words import write_count_words
+from ._internals.write_count_words import write_count_words
 
+# def read_all_lines():
+#     all_lines = []
+#     input_files_list = os.listdir("data/input/")
 
-def read_all_lines():
-    all_lines = []
-    input_files_list = os.listdir("data/input/")
-
-    for filename in input_files_list:
-        with open(filename, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            all_lines.extend(lines)
-    return all_lines
+#     for filename in input_files_list:
+#         with open(filename, "r", encoding="utf-8") as f:
+#             lines = f.readlines()
+#             all_lines.extend(lines)
+#     return all_lines
 
 
 def main():
 
     ##read all lines
+    all_lines = []
     input_files_list = os.listdir("data/input/")
-
-    # input_files_list = os.listdir("data/input/")
+    for filename in input_files_list:
+        file_path = os.path.join("data", "input", filename)
+        with open(file_path, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+    return all_lines
 
     ### preprocess lines
+    all_lines = [line.strip().lower() for line in all_lines]
+
     ### split in words
+    words = []
+    for line in all_lines:
+        words.extend(word.strip(",.!?") for word in line.split())
+
     ### count words
+    counter = {}
+    for word in words:
+        counter[word] = counter.get(word, 0) + 1
 
     # count the frequency of the words in the files in the input directory
     counter = {}
@@ -34,9 +47,6 @@ def main():
                 for w in l.split():
                     w = w.lower().strip(",.!?")
                     counter[w] = counter.get(w, 0) + 1
-
-    ###
-    # create the directory output/ if it doesn't exist
 
     write_count_words(counter)
 
